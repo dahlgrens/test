1a3b8dcb790257e503af7a2ada94d664679709f0
Siri Dahlgren <dahlgrenen@live.se>
2023-08-19 13:14:16 +0200

    fuxed meny

--- src/Components/MenyBotton.jsx
+++ src/Components/MenyBotton.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function MenyBotton({ toggleNavbar, isNavbarVisible }) {
+export default function MenyBotton({ toggleNavbar }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleClick = () => {
     toggleNavbar();
+    setIsOpen(!isOpen);
   };
 
   return (
     <div className="MenyBotton">
       <button className="menu-button" onClick={handleClick}>
-        {isNavbarVisible ? "Stäng" : "Meny"}
+        {isOpen ? "Stäng" : "Meny"}
       </button>
     </div>
   );
