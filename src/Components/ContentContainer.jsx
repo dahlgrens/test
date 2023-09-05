31e9635712dc6b6f9d68cb7d8d4b9b2b501cc3dc
Siri Dahlgren <dahlgrenen@live.se>
2023-09-05 18:45:17 +0200

    removed unused code

--- src/Components/ContentContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ContentContainer() {
-    return (
-        <div className="content-container">
-            <p>Welcome to our website! We provide information about various psychological diagnoses.</p>
-        </div>
-    );
-}
